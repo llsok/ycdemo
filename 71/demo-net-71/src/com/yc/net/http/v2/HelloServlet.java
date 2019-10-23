78df5bd1f3b541773096579278ee4a91c5d27947
Administrator <Administrator@PC-20120726LTQC>
2019-10-23 20:38:56 +0800

    71班：实现了响应重定向

--- 71/demo-net-71/src/com/yc/net/http/v2/HelloServlet.java
+++ 71/demo-net-71/src/com/yc/net/http/v2/HelloServlet.java
@@ -5,6 +5,17 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		// ????
+		
+		/**
+		 * 1、页面跳转（请求转发、响应重定向）
+		 * 
+		 * 2、直接输出 html 内容   或者  json 数据
+		 * 
+		 */
+		
+		response.sendRedirect("index.html");
+		
+		
 	}
 
 	@Override
--- 71/demo-net-71/src/com/yc/net/http/v2/HttpServletResponse.java
+++ 71/demo-net-71/src/com/yc/net/http/v2/HttpServletResponse.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class HttpServletResponse {
 	
@@ -18,6 +21,8 @@ public class HttpServletResponse {
 	//  状态码的描述信息
 	private String message="OK";
 	
+	private HashMap<String,String> headerMap = new HashMap<>();
+	
 	public HttpServletResponse(HttpServletRequest request, OutputStream out) {
 		super();
 		this.request = request;
@@ -25,9 +30,11 @@ public class HttpServletResponse {
 	}
 
 
-	/*response.setContentType("?????");  // 设置响应类型
-	response.setStatus(404,""); // 设置结果码
-*/	
+	/*
+	 * 	response.setContentType("?????");  // 设置响应类型
+		response.setStatus(404,""); // 设置结果码
+		response.setHeader("键","值")
+	*/	
 	// 提交方法
 	public void commit() throws IOException{
 		
@@ -36,30 +43,38 @@ public class HttpServletResponse {
 		
 		// 从 web.xml 文件中取 contentType， 替代之前的硬编码判断
 		String contentType = webXmlParser.getContentType(suffix);
+		// 设置 响应类型
+		setContentType(contentType);
 		
 		String resp = "HTTP/1.1 "+status+" "+message+"\r\n";
-		resp += "Content-Type: "+contentType+"\r\n";
+		//resp += "Content-Type: "+contentType+"\r\n";
+		// 写头域信息
+		for(Entry<String, String> entry : headerMap.entrySet()){
+			resp += entry.getKey() + ": " + entry.getValue() + "\r\n";
+		}
 		resp += "\r\n";
 		out.write(resp.getBytes());
 		
-		
-		String rootPath = "/Tomcat/webapps/photo";
-		String filePath = request.getRequestURL();
-		// 判断访问文件是否存在			
-		String diskPath = rootPath + filePath;
-		if(new File(diskPath).exists() == false){
-			diskPath = rootPath + "/404.html";
+		// 响应重定向不需要写 body 
+		if(status < 300 || status > 399){
+			String rootPath = "/Tomcat/webapps/photo";
+			String filePath = request.getRequestURL();
+			// 判断访问文件是否存在			
+			String diskPath = rootPath + filePath;
+			if(new File(diskPath).exists() == false){
+				diskPath = rootPath + "/404.html";
+			}
+			
+			FileInputStream fis = new FileInputStream(diskPath);
+			
+			byte[] buf = new byte[1024];
+			int count;
+			// 向浏览器发送报文
+			while((count = fis.read(buf))>0){
+				out.write(buf, 0, count);
+			}
+			fis.close();
 		}
-		
-		FileInputStream fis = new FileInputStream(diskPath);
-		
-		byte[] buf = new byte[1024];
-		int count;
-		// 向浏览器发送报文
-		while((count = fis.read(buf))>0){
-			out.write(buf, 0, count);
-		}
-		fis.close();
 		
 	}
 
@@ -67,6 +82,33 @@ public class HttpServletResponse {
 	public void setStatus(int status, String message) {
 		this.status = status;
 		this.message = message;
+	}
+
+	/**
+	 * 响应重定向
+	 * @param webPath
+	 */
+	public void sendRedirect(String webPath) {
+		
+		/**
+		 * 响应结果码： 
+		 * 	1xx 接受请求，继续处理
+		 * 	2xx	正常响应 200
+		 * 	3xx	响应重定向 301 302
+		 * 	4xx 浏览器端错误 404  405
+		 * 	5xx 服务器端错误
+		 */
+		this.setStatus(301, "Redirect");
+		this.addHeader("Location", webPath);
+		
+	}
+	
+	public void addHeader(String key, String value){
+		this.headerMap.put(key, value);
+	}
+	
+	public void setContentType(String contentType){
+		this.headerMap.put("Content-Type", contentType);
 	}
 
 }
