bb663cb5a661a95177f306ac7e778160441e5db7
Administrator <Administrator@516pc57>
2019-10-31 21:14:02 +0800

    71班：新增记录与事务管理

--- 71/demo-mybatis-71/src/test/java/com/yc/damai/dao/ProductMapperTest.java
+++ 71/demo-mybatis-71/src/test/java/com/yc/damai/dao/ProductMapperTest.java
@@ -2,12 +2,14 @@ package com.yc.damai.dao;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,6 +36,7 @@ public class ProductMapperTest {
 		List<Product> list = session.selectList("com.yc.damai.dao.ProductMapper.selectAll");
 		System.out.println(list.size());
 		System.out.println(list.get(0));
+		session.close();
 	}
 	
 	
@@ -41,6 +44,50 @@ public class ProductMapperTest {
 	public void testSelectByPid() throws IOException{
 		Product p = session.selectOne("com.yc.damai.dao.ProductMapper.selectByPid", 56);
 		System.out.println(p);
+		session.close();
 	}
+	
+	@Test
+	public void testInsert() throws IOException{
+		
+		// 数据库事务管理代码：成功则提交，失败就回滚
+		
+		try{
+			Product p = new Product();
+			p.setPname("男秋装");
+			p.setMarketPrice(110d);
+			p.setShopPrice(90d);
+			p.setImage("01.jpg");
+			p.setPdesc("好衣服");
+			p.setIsHot(1);
+			Timestamp t = new Timestamp(System.currentTimeMillis());
+			p.setPdate(t);
+			int count = session.insert("com.yc.damai.dao.ProductMapper.create", p);
+			
+			Assert.assertEquals(1, count);
+			
+			// 提交
+			session.commit();
+			
+		} catch (Exception e){
+			e.printStackTrace();
+			// 回滚
+			session.rollback();
+		} finally {
+			// 关闭会话
+			session.close();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
