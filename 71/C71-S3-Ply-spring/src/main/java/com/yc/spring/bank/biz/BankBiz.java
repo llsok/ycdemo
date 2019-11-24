d9b88ffcb0dc2843726c853da8d2a63169dd898c
Administrator <Administrator@516pc57>
2019-11-24 17:18:58 +0800

    71班：Spring事务配置

--- 71/C71-S3-Ply-spring/src/main/java/com/yc/spring/bank/biz/BankBiz.java
+++ 71/C71-S3-Ply-spring/src/main/java/com/yc/spring/bank/biz/BankBiz.java
@@ -21,18 +21,34 @@ public class BankBiz {
 	 * @throws BizException 
 	 */
 	public void diposit(int accountid, float money) throws BizException {
-
-		// 查找账号
-		if (adao.selectById(accountid) == null) {
-			throw new BizException("账号不存在：" + accountid);
-		}
-
-		// 存入金额
-		adao.update(accountid, money);
-
-		// 记录流水
-		odao.insert(accountid, money);
+		// 编程式事务处理
+		/*try{*/
+		
+		
+			// 查找账号
+			if (adao.selectById(accountid) == null) {
+				throw new BizException("账号不存在：" + accountid);
+			}
+			// 存入金额
+			adao.update(accountid, money);
+			
+			boolean b = true;
+			// int i=1/0;
+			if(b){
+				throw new BizException("我要抛出的异常（编译期）");
+			}
+			
+			// 记录流水
+			odao.insert(accountid, money);
+			// 没有异常就提交
+			
+			
+		/*} catch (Exception e ){
+			// 有异常就回滚
+		} finally {
+			// 最后都要关系连接
+		}*/
 
 	}
-
+	
 }
