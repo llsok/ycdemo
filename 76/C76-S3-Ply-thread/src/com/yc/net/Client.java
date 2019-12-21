87e22f4cbfe4e1bf85ed1f8cf41e374af2dfdb8c
2505 <306529917@qq.com>
2019-12-21 18:05:53 +0800

    1

--- 76/C76-S3-Ply-thread/src/com/yc/net/Client.java
+++ 76/C76-S3-Ply-thread/src/com/yc/net/Client.java
@@ -1,5 +1,10 @@
 package com.yc.net;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,6 +15,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
+
+	public static final String FILE_HOME = "f:/";
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 
@@ -34,7 +41,13 @@ public class Client {
 						int count;
 						count = in.read(buffer);
 						String s = new String(buffer, 0, count);
-						System.out.println(s);
+						if (s.startsWith("文件：")) {
+							String filename = s.substring("文件：".length());
+							filename = filename.substring(filename.lastIndexOf("/") + 1);
+							saveFile(in, filename);
+						} else {
+							System.out.println(s);
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -48,7 +61,13 @@ public class Client {
 			public void run() {
 				while (true) {
 					try {
-						out.write(sc.nextLine().getBytes());
+						String msg = sc.nextLine();
+						out.write(msg.getBytes());
+						// 文件：e:/a.html
+						if (msg.startsWith("文件：")) {
+							String filename = msg.substring("文件：".length());
+							sendFile(out, filename);
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -57,6 +76,46 @@ public class Client {
 			}
 		}.start();
 
+	}
+
+	static void saveFile(InputStream in, String filename) throws IOException {
+		// 读取发送过来的文件大小
+		DataInputStream dis = new DataInputStream(in);
+		long filesize = dis.readLong();
+		// 定义文件输出流
+		FileOutputStream fos = new FileOutputStream(FILE_HOME + filename);
+		try {
+			byte[] buf = new byte[1024];
+			int count;
+			int readsize = 0;
+			while (readsize < filesize && (count = in.read(buf)) > 0) {
+				fos.write(buf, 0, count);
+				readsize += count;
+			}
+		} finally {
+			fos.close();
+		}
+		System.out.println("文件保存成功：" + FILE_HOME + filename);
+	}
+
+	static void sendFile(OutputStream out, String filepath) throws IOException {
+		// 发送文件大小，好让对方的流知道文件何时结束文件的接收
+		DataOutputStream dos = new DataOutputStream(out);
+		File f = new File(filepath);
+		dos.writeLong(f.length());
+		
+		// 将文件发送给对方
+		FileInputStream fis = new FileInputStream(f);
+		try {
+			byte[] buf = new byte[1024];
+			int count;
+			while ((count = fis.read(buf)) > 0) {
+				out.write(buf, 0, count);
+			}
+		} finally {
+			fis.close();
+		}
+		System.out.println("文件发送成功：" + filepath);
 	}
 
 }
--- 76/C76-S3-Ply-thread/src/com/yc/net/Server.java
+++ 76/C76-S3-Ply-thread/src/com/yc/net/Server.java
@@ -1,5 +1,10 @@
 package com.yc.net;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,6 +18,8 @@ import java.util.Scanner;
  * Socket 套接字，两台计算机的输入输出流的对接
  */
 public class Server {
+
+	public static final String FILE_HOME = "f:/";
 
 	public static void main(String[] args) throws IOException {
 
@@ -41,7 +48,13 @@ public class Server {
 						int count;
 						count = in.read(buffer);
 						String s = new String(buffer, 0, count);
-						System.out.println(s);
+						if (s.startsWith("文件：")) {
+							String filename = s.substring("文件：".length());
+							filename = filename.substring(filename.lastIndexOf("/") + 1);
+							saveFile(in, filename);
+						} else {
+							System.out.println(s);
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -49,13 +62,19 @@ public class Server {
 
 			}
 		}.start();
-		
+
 		// 写的线程
 		new Thread() {
 			public void run() {
 				while (true) {
 					try {
-						out.write(sc.nextLine().getBytes());
+						String msg = sc.nextLine();
+						out.write(msg.getBytes());
+						// 文件：e:/a.html
+						if (msg.startsWith("文件：")) {
+							String filename = msg.substring("文件：".length());
+							sendFile(out, filename);
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -63,6 +82,46 @@ public class Server {
 
 			}
 		}.start();
+
 	}
 
+	static void saveFile(InputStream in, String filename) throws IOException {
+		// 读取发送过来的文件大小
+		DataInputStream dis = new DataInputStream(in);
+		long filesize = dis.readLong();
+		// 定义文件输出流
+		FileOutputStream fos = new FileOutputStream(FILE_HOME + filename);
+		try {
+			byte[] buf = new byte[1024];
+			int count;
+			int readsize = 0;
+			while (readsize < filesize && (count = in.read(buf)) > 0) {
+				fos.write(buf, 0, count);
+				readsize += count;
+			}
+		} finally {
+			fos.close();
+		}
+		System.out.println("文件保存成功：" + FILE_HOME + filename);
+	}
+
+	static void sendFile(OutputStream out, String filepath) throws IOException {
+		// 发送文件大小，好让对方的流知道文件何时结束文件的接收
+		DataOutputStream dos = new DataOutputStream(out);
+		File f = new File(filepath);
+		dos.writeLong(f.length());
+
+		// 将文件发送给对方
+		FileInputStream fis = new FileInputStream(f);
+		try {
+			byte[] buf = new byte[1024];
+			int count;
+			while ((count = fis.read(buf)) > 0) {
+				out.write(buf, 0, count);
+			}
+		} finally {
+			fis.close();
+		}
+		System.out.println("文件发送成功：" + filepath);
+	}
 }
