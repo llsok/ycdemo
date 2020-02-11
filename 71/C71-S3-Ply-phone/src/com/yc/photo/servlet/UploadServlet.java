9ca7dbac398eaecd57a1947ad4c8d384b9074628
liaoyan <liaoyan@chinasofti.com>
2020-02-11 19:11:57 +0800

    Jquery实现文件上传

--- 71/C71-S3-Ply-phone/src/com/yc/photo/servlet/UploadServlet.java
+++ 71/C71-S3-Ply-phone/src/com/yc/photo/servlet/UploadServlet.java
@@ -1,6 +1,8 @@
 package com.yc.photo.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +17,16 @@ public class UploadServlet extends HttpServlet {
 		Part part = request.getPart("file");
 		String filename = part.getSubmittedFileName();
 		
-		part.write("d:/" + filename);
+		/**
+		 * 上传图片到 展示目录下
+		 */
+		String webpath = "/images/fulls";
+		// 获取应用上下文对象
+		ServletContext application = this.getServletContext();
+		// 将web路径转成磁盘路径
+		String diskpath = application.getRealPath(webpath);
+		
+		part.write(diskpath + "/" +filename);
 		
 		response.getWriter().append("success");
 		
