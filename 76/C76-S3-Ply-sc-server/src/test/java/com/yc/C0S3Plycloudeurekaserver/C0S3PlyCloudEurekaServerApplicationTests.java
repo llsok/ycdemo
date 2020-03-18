4707c7d9b202c9905120299b1d47a60599b81026
6606 <306529917@qq.com>
2020-03-18 17:36:57 +0800

    SpringCloud: 服务注册与发现2

---
+++ 76/C76-S3-Ply-sc-server/src/test/java/com/yc/C0S3Plycloudeurekaserver/C0S3PlyCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C0S3Plycloudeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C0S3PlyCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
