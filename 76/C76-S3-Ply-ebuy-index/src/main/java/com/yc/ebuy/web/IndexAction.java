4821f784d7cfad4f613b8662f982fa12e36c89e8
6606 <306529917@qq.com>
2020-03-22 12:05:01 +0800

    门户测试MyBatis接口

--- 76/C76-S3-Ply-ebuy-index/src/main/java/com/yc/ebuy/web/IndexAction.java
+++ 76/C76-S3-Ply-ebuy-index/src/main/java/com/yc/ebuy/web/IndexAction.java
@@ -1,11 +1,14 @@
 package com.yc.ebuy.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yc.ebuy.bean.EasybuyProductCategoryExample;
 import com.yc.ebuy.dao.EasybuyProductCategoryMapper;
 
 @RestController
@@ -16,6 +19,14 @@ public class IndexAction {
 
 	@GetMapping({ "/", "index", "index.html" })
 	public ModelAndView index(ModelAndView mav) {
+		
+		EasybuyProductCategoryExample pce = new EasybuyProductCategoryExample();
+		pce.createCriteria().andTypeEqualTo(1);
+		
+		List<?> list = pcm.selectByExample(pce);
+		
+		mav.addObject("pclist", list);
+		
 		mav.setViewName("index");
 		return mav;
 	}
